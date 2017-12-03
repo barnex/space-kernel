772255101c1c359f01c8f3efaf0c7ffe40893729
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2017-12-03 14:56:17 +0100

    start high-performance integrator

--- integrator.go
+++ integrator.go
@@ -1,24 +1,50 @@
 package main
 
+func NewIntegrator(a AccelFunc, dtheta float64) *Integrator {
+	return &Integrator{
+		acc:    a,
+		dtheta: dtheta,
+		dt:     1e-5, // TODO
+	}
+}
+
+type Integrator struct {
+	T    float64
+	P, V Vec
+
+	acc    AccelFunc
+	dtheta float64
+	dt     float64
+}
+
+func (s *Integrator) Advance(duration float64) {
+	p1, v1, dt1 := Integrate(AVerlet, s.P, s.V, s.acc, s.T, s.T+duration, s.dt, s.dtheta)
+	s.P = p1
+	s.V = v1
+	s.dt = dt1
+}
+
 // Integrate advances position and velocity given an acceleration function,
 // stepping from t0 to tmax with initial time step dt0 and angle step d theta.
-func Integrate(s StepperFunc, p0, v0 Vec, a AccelFunc, t0, tmax, dt0, dth float64) (Vec, Vec) {
+func Integrate(s StepperFunc, p0, v0 Vec, a AccelFunc, t0, tmax, dt0, dth float64) (Vec, Vec, float64) {
 	t := t0
 	dt := dt0
 	p := p0
 	v := v0
 
+	var dt2 float64
 	for t+dt < tmax {
-		var dt2 float64
 		p, v, dt2 = s(p, v, a, t, dt, dth)
 		t += dt
 		dt = dt2
 	}
 	dt = tmax - t
-	p, v, _ = s(p, v, a, t, dt, dth)
-	t += dt
+	if dt != 0 {
+		p, v, _ = s(p, v, a, t, dt, dth)
+		t += dt
+	}
 
-	return p, v
+	return p, v, dt2
 }
 
 // An AccelFunc function returns acceleration (m/s2) as a function of position and time.
--- integrator_test.go
+++ integrator_test.go
@@ -5,6 +5,52 @@ import (
 	"testing"
 )
 
+func TestIntegrator(t *testing.T) {
+	cases := []struct {
+		p0, v0       Vec
+		dtheta       float64
+		duration     float64
+		pWant, vWant Vec
+		tol          float64
+	}{
+		// tolerances drop quadratically with angle step
+		{Vec{0, 1}, Vec{1, 0}, 1e-1, math.Pi / 2, Vec{1, 0}, Vec{0, -1}, 3e-3},
+		{Vec{0, 1}, Vec{1, 0}, 1e-2, math.Pi / 2, Vec{1, 0}, Vec{0, -1}, 3e-5},
+		{Vec{0, 1}, Vec{1, 0}, 1e-3, math.Pi / 2, Vec{1, 0}, Vec{0, -1}, 3e-7},
+		{Vec{0, 1}, Vec{1, 0}, 1e-4, math.Pi / 2, Vec{1, 0}, Vec{0, -1}, 3e-9},
+		{Vec{0, 1}, Vec{1, 0}, 1e-5, math.Pi / 2, Vec{1, 0}, Vec{0, -1}, 3e-11},
+		// even smaller steps don't increase convergence anymore due to truncation.
+	}
+
+	for i, c := range cases {
+		s := NewIntegrator(UnitAcc, c.dtheta)
+		s.P = c.p0
+		s.V = c.v0
+		s.Advance(c.duration)
+		if err := s.P.Sub(c.pWant).Len(); err > c.tol || math.IsNaN(err) {
+			t.Errorf("case %v: p: have %v, want %v +/- %v, err=%v", i, s.P, c.pWant, c.tol, err)
+		}
+		//if err := s.V.Sub(c.vWant).Len(); err > c.tol || math.IsNaN(err) {
+		//	t.Errorf("case %v: v: have %v, want %v +/- %v, err=%v", i, s.V, c.vWant, c.tol, err)
+		//}
+	}
+}
+
+func BenchmarkIntegrator(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := NewIntegrator(UnitAcc, 1e-3)
+		s.P = Vec{1, 0}
+		s.V = Vec{0, 1}
+		s.Advance(2 * math.Pi)
+	}
+}
+
+func BenchmarkIntegrate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Integrate(AVerlet, Vec{1, 0}, Vec{0, 1}, UnitAcc, 0, 2*math.Pi, 1e-5, 1e-3)
+	}
+}
+
 func TestAVerlet1(t *testing.T) {
 	cases := []struct {
 		p0, v0   Vec
@@ -25,8 +71,8 @@ func TestAVerlet1(t *testing.T) {
 	stepper := AVerlet
 
 	for i, c := range cases {
-		p, _ := Integrate(stepper, c.p0, c.v0, UnitAcc, 0, c.duration, 1e-6, c.dtheta)
-		if err := p.Sub(c.pWant).Len(); err > c.tol {
+		p, _, _ := Integrate(stepper, c.p0, c.v0, UnitAcc, 0, c.duration, 1e-6, c.dtheta)
+		if err := p.Sub(c.pWant).Len(); err > c.tol || math.IsNaN(err) {
 			t.Errorf("case %v: have %v, want %v +/- %v, err=%v", i, p, c.pWant, c.tol, err)
 		}
 	}
@@ -52,7 +98,7 @@ func TestVerlet1(t *testing.T) {
 	stepper := Verlet
 
 	for i, c := range cases {
-		p, _ := Integrate(stepper, c.p0, c.v0, UnitAcc, 0, c.duration, c.dt, 0)
+		p, _, _ := Integrate(stepper, c.p0, c.v0, UnitAcc, 0, c.duration, c.dt, 0)
 		if err := p.Sub(c.pWant).Len(); err > c.tol {
 			t.Errorf("case %v: have %v, want %v +/- %v, err=%v", i, p, c.pWant, c.tol, err)
 		}
@@ -79,8 +125,8 @@ func TestSymEuler1(t *testing.T) {
 	stepper := SymEuler
 
 	for i, c := range cases {
-		p, _ := Integrate(stepper, c.p0, c.v0, UnitAcc, 0, c.duration, c.dt, 0)
-		if err := p.Sub(c.pWant).Len(); err > c.tol {
+		p, _, _ := Integrate(stepper, c.p0, c.v0, UnitAcc, 0, c.duration, c.dt, 0)
+		if err := p.Sub(c.pWant).Len(); err > c.tol || math.IsNaN(err) {
 			t.Errorf("case %v: have %v, want %v +/- %v, err=%v", i, p, c.pWant, c.tol, err)
 		}
 	}
